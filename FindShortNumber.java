307504cf6acde45a7f9b44fb95d9ab16a7fae2e3
rajeshkumarmolugu <rajeshkumar.molugu@gmail.com>
2025-02-24 15:55:47 -0500

    First Commit

---
+++ FindShortNumber.java
@@ -0,0 +1,13 @@
+public class FindShortNumber {
+    public static void main(String[] args) {
+        int[] num = {23,24,5,6,12,3,4};
+        int temp = num[0];
+
+        for(int i=1; i<num.length;i++){
+            if(num[i] < temp){
+                temp = num[i];
+            }
+        }
+        System.out.println("Smallest number is " + temp);
+    }
+}
